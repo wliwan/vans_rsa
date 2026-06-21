59ce224259291ea1023e00a84d84edde4ef54e01
Vance <vance@localhost.localdomain>
2026-06-21 17:10:01 +0800

    微调样式

--- web/src/layout/components/sidebar/components/SideLogo.vue
+++ web/src/layout/components/sidebar/components/SideLogo.vue
@@ -1,15 +1,15 @@
 <template>
   <router-link h-60 f-c-c to="/">
-    <icon-custom-logo text-36 color-primary></icon-custom-logo>
+    <icon-custom-logo color-primary style="font-size: 15rem"></icon-custom-logo>
     <h2
       v-show="!appStore.collapsed"
       ml-2
       mr-8
       max-w-150
       flex-shrink-0
-      text-16
       font-bold
       color-primary
+      style="font-size: 6rem"
     >
       {{ title }}
     </h2>
